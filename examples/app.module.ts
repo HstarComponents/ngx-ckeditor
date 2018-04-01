e16514370e852477a108990eaa64325fa32fb8f8
Hstarorg <hm910705@163.com>
2018-04-01 09:58:28 +0800

    Update demo

--- examples/app.module.ts
+++ examples/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CKEditorModule } from '../src';
@@ -7,7 +8,7 @@ import { AppComponent } from './app/app.component';
 import { PAGES } from './pages';
 
 @NgModule({
-  imports: [BrowserModule, CKEditorModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, CKEditorModule, AppRoutingModule],
   declarations: [AppComponent, ...PAGES],
   providers: [
     /* TODO: Providers go here */
--- examples/pages/reactive-form-demo/reactive-form-demo.component.ts
+++ examples/pages/reactive-form-demo/reactive-form-demo.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-	selector: 'reactive-form-demo',
-	templateUrl: 'reactive-form-demo.component.html'
+  selector: 'reactive-form-demo',
+  templateUrl: 'reactive-form-demo.component.html'
 })
-
 export class ReactiveFormDemoComponent implements OnInit {
+  heroForm: FormGroup;
+  language = 'en';
+  editorValue = '';
+  constructor(private fb: FormBuilder) {}
 
-	ngOnInit() { }
+  ngOnInit() {
+    this.heroForm = this.fb.group({
+      html: ''
+    });
+  }
 }
