ca2deeead6f58014199d2be02117fe79d4b87346
Hstarorg <hm910705@163.com>
2018-10-11 23:31:21 +0800

    dynamic config demo

---
+++ examples/pages/dynamic-config-demo/dynamic-config-demo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dynamic-config-demo',
+  templateUrl: 'dynamic-config-demo.component.html'
+})
+export class DynamicConfigDemoComponent implements OnInit {
+  public editorValue: string = '';
+
+  public editorConfig: any = {
+    forceEnterMode: true,
+    height: 250
+  };
+
+  public editorConfigStr: string = JSON.stringify(this.editorConfig);
+
+  ngOnInit() {}
+
+  public updateEditorConfig() {
+    try {
+      this.editorConfig = JSON.parse(this.editorConfigStr);
+    } catch (e) {
+      alert('Invalid json config.');
+    }
+  }
+}
--- examples/pages/index.ts
+++ examples/pages/index.ts
@@ -1,7 +1,8 @@
 import { BasicDemoComponent } from './basic-demo/basic-demo.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { ReactiveFormDemoComponent } from './reactive-form-demo/reactive-form-demo.component';
+import { DynamicConfigDemoComponent } from './dynamic-config-demo/dynamic-config-demo.component';
 
-export { BasicDemoComponent, ReactiveFormDemoComponent, NotfoundComponent };
+export { BasicDemoComponent, ReactiveFormDemoComponent, NotfoundComponent, DynamicConfigDemoComponent };
 
-export const PAGES = [BasicDemoComponent, ReactiveFormDemoComponent, NotfoundComponent];
+export const PAGES = [BasicDemoComponent, ReactiveFormDemoComponent, NotfoundComponent, DynamicConfigDemoComponent];
